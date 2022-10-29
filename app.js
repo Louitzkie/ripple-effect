a2f57670a7a8f61083a541a3bac4e51183e5b434
louis <louis_cruz10@yahoo.com>
2022-10-29 17:43:58 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const btns = document.querySelectorAll(".ripple");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const buttonLeft = e.target.offsetLeft;
+    const buttonTop = e.target.offsetTop;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.left = xInside + "px";
+    circle.style.top = yInside + "px";
+    btn.appendChild(circle);
+  });
+});
